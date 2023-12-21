16866eda316470b9272acf8ec4031c77b3b979dd
kyle <40372828+kylewong791@users.noreply.github.com>
2023-12-21 03:10:16 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+// Smooth scrolling
+$(document).ready(function () {
+    $(document).on('click', 'a[href^="#"]', function (event) {
+        event.preventDefault();
+
+        var targetSection = $($.attr(this, 'href'));
+        var navbarHeight = $('.navbar').height();
+        var targetPosition = targetSection.offset().top - navbarHeight;
+
+        // Explicitly specify the listener as passive
+        $('html, body').animate({
+            scrollTop: targetPosition
+        }, 500);
+    });
+});
+
+
+// Make the navigation bar sticky
+$(document).ready(function () {
+    var navbar = $('.navbar');
+    var navbarHeight = navbar.height();
+    var meSection = $('#me');
+
+    // Adjust margin-top for sections to make space for the sticky navbar
+    var sections = $('.scroll-section'); // Define sections here
+    sections.css('margin-top', navbarHeight + 'px');
+
+    $(window).on('scroll', function () {
+        var scrollTop = $(window).scrollTop();
+        var meSectionTop = meSection.offset().top;
+
+        // Check if the scroll position is below the me section
+        if (scrollTop > meSectionTop) {
+            navbar.addClass('sticky');
+        } else {
+            navbar.removeClass('sticky');
+        }
+    });
+});
+
+// Fade in/out
+$(document).ready(function() {
+    // Function to check if an element is in the viewport
+    function isElementInView(element) {
+        var elementTop = $(element).offset().top;
+        var elementBottom = elementTop + $(element).outerHeight();
+        var viewportTop = $(window).scrollTop();
+        var viewportBottom = viewportTop + $(window).height();
+        return elementBottom > viewportTop && elementTop < viewportBottom;
+    }
+
+    // Check for visibility of each section on scroll
+    $(window).on('scroll', function() {
+        $('.scroll-section').each(function() {
+            if (isElementInView(this)) {
+                $(this).addClass('in-view');
+            } else {
+                $(this).removeClass('in-view');
+            }
+        });
+    });
+});
+
+
+// Flip "cards"
+$(document).ready(function() {
+    $('.project-item').click(function() {
+        $(this).find('.card').toggleClass('flipped');
+    });
+});
+
